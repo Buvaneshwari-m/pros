62bde7dc7a1579e45da8d4050eb8a27a474a8c79
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-08-03 21:44:12 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+nos= int(input())
+l = list(map(str,input().split()))
+mov=[]
+for ioo in range(nos):
+    if int(l[ioo])==ioo
+        mov.append(l[ioo])
+if(len(mov)==0):
+    print(-1)
+else:
+    print(" ".join(mov))
+
